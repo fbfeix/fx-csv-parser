0ecdd102a27971a971440c932d72784ee1b00846
Felix Astner <me@felixastner.com>
2023-02-20 19:01:57 +0100

    test tripplet quotes and comma

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "src/CSVParser.h"
 
 int main() {
-    std::filesystem::path path = "/home/felix/CLionProjects/FxCSVParser/examples/data/index.csv";
+    std::filesystem::path path = "/home/felix/CLionProjects/FxCSVParser/examples/data/properties.csv";
     path = absolute(path);
     CSVParser parser(path, ',');
     auto file = parser.parse();
